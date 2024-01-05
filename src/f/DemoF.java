8ad523c8e8c6eafbf409dd4b9ddc0704b36498c1
haithemalsaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-05 14:24:08 -0500

    intiate push

---
+++ src/f/DemoF.java
@@ -0,0 +1,27 @@
+package f;
+
+public class DemoF {
+	
+	    public static void main(String[] args) {
+	        DemoF tester = new DemoF();
+	        System.out.println(tester.repeatFront("abcdef", 3));  // Output: "abcabca"
+	        System.out.println(tester.repeatFront("Java", 2));    // Output: "JaJ"
+	        System.out.println(tester.repeatFront("hello", 0));   // Output: ""
+	    }
+
+	    public String repeatFront(String str, int n) {
+	        String result = "";
+
+	        if (n == 0) {
+	            result = "";
+	        } else {
+	            for (int i = n; i >= 0; i--) {
+	                result += str.substring(0, i);
+	            }
+	        }
+
+	        return result;
+	    }
+	}
+
+
